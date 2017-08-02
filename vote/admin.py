000dafd4ea90b30606272ba268c0dbed68d086df
Joseph Mutumi Ngambeki <jjmutumi@gmail.com>
2017-08-02 15:46:10 +0300

    Up to admin part 4

--- vote/admin.py
+++ vote/admin.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["text", "publish_date", "choice_count"]
+
+    def choice_count(self, obj):
+        return obj.choice_set.count()
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["votes", "text", "question"]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- vote/models.py
+++ vote/models.py
@@ -7,7 +7,13 @@ class Question(models.Model):
     text = models.CharField(max_length=200)
     publish_date = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.text
+
 class Choice(models.Model):
     text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return "{} : {}".format(self.votes, self.text)
